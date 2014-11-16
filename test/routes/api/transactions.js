c9773c5ee75c5d4fc19b6b4c5e7d74b02ce13bc9
gwilliamson <gwilliamson@zephyrhealthinc.com>
2014-11-16 15:47:37 -0800

    supertest and two tests for transactions api

--- test/routes/api/transactions.js
+++ test/routes/api/transactions.js
@@ -1,12 +1,29 @@
 /**
  * Created by gwilliamson on 11/16/14.
  */
-var assert = require("assert")
-describe('Array', function(){
-    describe('#indexOf()', function(){
-        it('should return -1 when the value is not present', function(){
-            assert.equal(-1, [1,2,3].indexOf(5))
-            assert.equal(-1, [1,2,3].indexOf(0))
+var request = require('supertest'),
+    app = require("../../../app")
+
+describe('Transactions API:', function() {
+
+    describe('GET /api/transactions', function() {
+        it('responds with json', function(done) {
+            request(app)
+                .get('/api/transactions')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200, done)
         })
     })
+
+    describe('GET /api/transactions/1', function() {
+        it('responds with json', function(done) {
+            request(app)
+                .get('/api/transactions/1')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200, done)
+        })
+    })
+
 })
